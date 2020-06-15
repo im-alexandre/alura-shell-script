3662f4a68dbda0db1390c578d3070fa4ec359ac8
im-alexandre <48188379+im-alexandre@users.noreply.github.com>
2020-06-15 12:30:50 -0300

    Update verificacao-memoria-consumida.sh

--- scripts/verificacao-memoria-consumida.sh
+++ scripts/verificacao-memoria-consumida.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+CAMINHO_LOG=/var/log
+cd $CAMINHO_LOG
+
+if [[ ! -f memoria.log ]]; then
+    echo "Data_hora|memoria_usada_gb|relacao_memoria_atual_total" > memoria.log
+fi
+
 memoria_usada_gb=$(free -h | grep -i mem | awk '{ print $3 }')
 memoria_total=$(free | grep -i mem | awk '{ print $2 }')
+memoria_total_gb=$(free -h | grep -i mem | awk '{ print $2 }')
 memoria_consumida=$(free | grep -i mem | awk '{ print $3 }')
 relacao_memoria_atual_total=$(bc <<< "scale=2;$memoria_consumida/$memoria_total *100" | awk -F. '{ print $1 }')
+data_hora=$(date +%F,%H:%M:%S)
+
+echo "$data_hora|$memoria_usada_gb|$relacao_memoria_atual_total" >> memoria.log
 
 if [[ $relacao_memoria_atual_total -gt 7 ]]; then
 mail -s "Consumo memória limite" im.alexandre07@gmail.com<<del
